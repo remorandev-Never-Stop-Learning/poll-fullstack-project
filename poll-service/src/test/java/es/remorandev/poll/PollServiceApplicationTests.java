c8d19c0306096dc70126ee2f8d1fdaff8a5f895b
Roberto Moran <remorandev@gmail.com>
2019-10-31 17:55:47 +0100

    -ADD proyecto base para back-end de aplicativo de encuestas

---
+++ poll-service/src/test/java/es/remorandev/poll/PollServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.remorandev.poll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
