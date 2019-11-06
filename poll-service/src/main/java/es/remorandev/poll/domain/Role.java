7798a7a66b86b19053574061344cf7e19122dde0
Roberto Moran <remorandev@gmail.com>
2019-11-06 11:16:39 +0100

    -FIX nombre de la tabla role para que concuerde los datos de la relación especificada en usuario

--- poll-service/src/main/java/es/remorandev/poll/domain/Role.java
+++ poll-service/src/main/java/es/remorandev/poll/domain/Role.java
@@ -5,7 +5,7 @@ import org.hibernate.annotations.NaturalId;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "role")
+@Table(name = "roles")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- poll-service/src/main/java/es/remorandev/poll/domain/User.java
+++ poll-service/src/main/java/es/remorandev/poll/domain/User.java
@@ -43,11 +43,10 @@ public class User extends DateAudit {
     @Size(max = 100)
     private String password;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id")
-    )
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
     public User() {
