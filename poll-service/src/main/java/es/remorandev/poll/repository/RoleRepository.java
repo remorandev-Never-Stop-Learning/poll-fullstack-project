71695906399a4f4acc94b7bac44b9c5e995752bb
Roberto Moran <remorandev@gmail.com>
2019-11-06 10:18:08 +0100

    -ADD JpaRespository para las entidades User y Role

---
+++ poll-service/src/main/java/es/remorandev/poll/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package es.remorandev.poll.repository;
+
+import es.remorandev.poll.domain.Role;
+import es.remorandev.poll.domain.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
