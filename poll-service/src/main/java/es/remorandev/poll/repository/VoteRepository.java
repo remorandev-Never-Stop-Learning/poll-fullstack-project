9e67091667f47bd1b29f0ed8dac5ac18585b4905
Roberto Moran <remorandev@gmail.com>
2019-11-13 13:12:56 +0100

    -ADD Repository para las clases Poll y Vote

---
+++ poll-service/src/main/java/es/remorandev/poll/repository/VoteRepository.java
@@ -0,0 +1,29 @@
+package es.remorandev.poll.repository;
+
+import es.remorandev.poll.model.ChoiceVoteCount;
+import es.remorandev.poll.model.Vote;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+    @Query("SELECT NEW es.remorandev.polls.model.ChoiceVoteCount(v.choice.id, count(v.id)) FROM Vote v WHERE v.poll.id in :pollIds GROUP BY v.choice.id")
+    List<ChoiceVoteCount> countByPollIdInGroupByChoiceId(@Param("pollIds") List<Long> pollIds);
+
+    @Query(value = "SELECT NEW es.remorandev.poll.model.ChoiceVoteCount(v.choice.id, count(v.id)) FROM Vote v WHERE v.poll.id = :pollId GROUP BY v.choice.id")
+    List<ChoiceVoteCount> countByPollIdGroupByChoiceId(@Param("pollId") Long pollId);
+
+    @Query(value = "SELECT v FROM Vote v WHERE v.user.id = :usereId AND v.poll.id = :pollId")
+    Optional<Vote> findByUserIdAndPollId(@Param("userId") Long userId, @Param("pollId") Long pollId);
+
+    @Query(value = "SELECT count(v.id) FROM Vote v WHERE v.user.id = :userId")
+    long countByUserId(@Param("userId") Long userId);
+
+    @Query(value = "SELECT v.poll.id FROM Vote v WHERE v.user.id = :userId")
+    Page<Long> findVotedPollIdsByUserId(@Param("userId") Long userid, Pageable pageable);
+}
