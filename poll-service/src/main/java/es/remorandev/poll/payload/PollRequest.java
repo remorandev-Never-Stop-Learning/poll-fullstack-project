88445dacf7162d4e8a409c49fd53620a463574fe
Roberto Moran <remorandev@gmail.com>
2019-11-14 18:21:32 +0100

    -ADD objetos a utilizar para las comunicaciones de los servicios REST

---
+++ poll-service/src/main/java/es/remorandev/poll/payload/PollRequest.java
@@ -0,0 +1,43 @@
+package es.remorandev.poll.payload;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+public class PollRequest {
+    @NotBlank
+    @Size(max = 140)
+    private String question;
+
+    private List<ChoiceRequest> choices;
+
+    @NotNull
+    @Valid
+    private PollLength pollLength;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public List<ChoiceRequest> getChoices() {
+        return choices;
+    }
+
+    public void setChoices(List<ChoiceRequest> choices) {
+        this.choices = choices;
+    }
+
+    public PollLength getPollLength() {
+        return pollLength;
+    }
+
+    public void setPollLength(PollLength pollLength) {
+        this.pollLength = pollLength;
+    }
+}
